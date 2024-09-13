c97faa2db216504b220267aef7f080e9d82839e8
Felipe Smania <felipesmania13@gmail.com>
2024-09-12 22:45:43 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -57,8 +57,24 @@ const metasRealizadas = async () => {
     }
    
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((metas) =>{
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select ({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -82,6 +98,10 @@ const start = async () => {
                     value:"realizadas"
                 },
                 {
+                    name:"Metas abertas",
+                    value:"abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -97,9 +117,12 @@ const start = async () => {
             case "listar":
                 await listarMetas()
                 break
-                case "realizadas":
-                    await metasRealizadas()
-                    break
+            case "realizadas":
+                await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
+                break
             case "sair":
                 console.log("Até a próxima!")
                 return
